7d08d65318b02c8c853300bab046538194b4186e
Lizzy Ell <lunar@lunarware.tech>
2024-04-13 05:26:54 +0000

    setup Next.JS Project and Adjust Templates

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Poppins } from "next/font/google";
+import './topstyle.css';
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata = {
+  title: "Uploadz",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
